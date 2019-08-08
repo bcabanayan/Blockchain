efc2ea05f49e7d3135dc1cf43ac2965a68a3e762
bcabanayan <bcabanayan@gmail.com>
2019-08-07 18:18:54 -0700

    Removed proof of work function from server

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -77,20 +77,20 @@ class Blockchain(object):
     def last_block(self):
         return self.chain[-1]
 
-    def proof_of_work(self, last_proof):
-        """
-        Simple Proof of Work Algorithm
-        Find a number p such that hash(last_block_string, p) contains 6 leading
-        zeroes
-        """
-        proof = 0
-        # for block 1, hash(1, p) = 000000x
-        # find value for proof that, when hashed with last block string, generates value with 6 leading 0s
-        # guess and check until you find the answer you need
-        while self.valid_proof(last_proof, proof) is False:
-            proof+= 1
+    # def proof_of_work(self, last_proof):
+    #     """
+    #     Simple Proof of Work Algorithm
+    #     Find a number p such that hash(last_block_string, p) contains 6 leading
+    #     zeroes
+    #     """
+    #     proof = 0
+    #     # for block 1, hash(1, p) = 000000x
+    #     # find value for proof that, when hashed with last block string, generates value with 6 leading 0s
+    #     # guess and check until you find the answer you need
+    #     while self.valid_proof(last_proof, proof) is False:
+    #         proof+= 1
 
-        return proof
+    #     return proof
         
 
     @staticmethod
